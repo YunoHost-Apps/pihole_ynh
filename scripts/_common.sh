8aa9c84268069c810f48f64a83d8fc76f9246e8e
Maniack Crudelis <maniackc_dev@crudelis.fr>
2018-09-30 20:15:37 +0200

    Panel-config + actions fully tested

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -31,6 +31,24 @@ IS_PACKAGE_CHECK () {
 }
 
 #=================================================
+# BOOLEAN CONVERTER
+#=================================================
+
+bool_to_01 () {
+	local var="$1"
+	[ "$var" = "true" ] && var=1
+	[ "$var" = "false" ] && var=0
+	echo "$var"
+}
+
+bool_to_true_false () {
+	local var="$1"
+	[ "$var" = "1" ] && var=true
+	[ "$var" = "0" ] && var=false
+	echo "$var"
+}
+
+#=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
