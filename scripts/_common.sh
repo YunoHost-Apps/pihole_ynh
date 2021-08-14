1603b2ccafdec8f1189d16480e5306d3a9315ebd
yalh76 <yalh@yahoo.com>
2021-08-14 18:42:20 +0200

    Remove CHECK_SIZE

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,19 +11,6 @@ pkg_dependencies="sqlite3 idn2 php$YNH_DEFAULT_PHP_VERSION-sqlite3"
 # PERSONAL HELPERS
 #=================================================
 
-CHECK_SIZE () {	# Vérifie avant chaque backup que l'espace est suffisant
-	file_to_analyse=$1
-	backup_size=$(du --summarize "$file_to_analyse" | cut -f1)
-	free_space=$(df --output=avail "/home/yunohost.backup" | sed 1d)
-
-	if [ $free_space -le $backup_size ]
-	then
-		ynh_print_err "Espace insuffisant pour sauvegarder $file_to_analyse."
-		ynh_print_err "Espace disponible: $(HUMAN_SIZE $free_space)"
-		ynh_die "Espace nécessaire: $(HUMAN_SIZE $backup_size)"
-	fi
-}
-
 #=================================================
 # PACKAGE CHECK BYPASSING...
 #=================================================
