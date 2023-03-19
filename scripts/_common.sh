ec61cfe2302e3fed8b20aaf2943fccb516f9f5b5
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-19 15:12:37 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,11 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-#REMOVEME? php_dependencies="php$YNH_DEFAULT_PHP_VERSION-common php$YNH_DEFAULT_PHP_VERSION-cgi php$YNH_DEFAULT_PHP_VERSION-sqlite3 php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-intl"
-
-# dependencies used by the app (must be on a single line)
-#REMOVEME? pkg_dependencies="cmake build-essential libgmp-dev libidn11-dev nettle-dev libreadline-dev sqlite3 cron curl iputils-ping psmisc unzip idn2 libcap2-bin dns-root-data libcap2 netcat-openbsd procps jq $php_dependencies"
-
 pihole_adminlte_version=5.18.4
 pihole_flt_version=5.21
 
