4cba5525355455b71cbb789625088e38d9107317
OniriCorpe <oniricorpe@disroot.org>
2024-02-13 23:58:39 +0100

    fix autoupdate

--- .github/workflows/updater.sh
+++ .github/workflows/updater.sh
@@ -18,10 +18,10 @@ current_version=$(cat manifest.json | jq -j '.version|split("~")[0]')
 repo=$(cat manifest.json | jq -j '.upstream.code|split("https://github.com/")[1]')
 # Some jq magic is needed, because the latest upstream release is not always the latest version (e.g. security patches for older versions)
 version=$(curl --silent "https://api.github.com/repos/$repo/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
-version_adminlte=$(curl --silent "https://api.github.com/repos/pi-hole/AdminLTE/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
+version_adminlte=$(curl --silent "https://api.github.com/repos/pi-hole/web/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
 version_ftl=$(curl --silent "https://api.github.com/repos/pi-hole/FTL/releases" | jq -r '.[] | select( .prerelease != true ) | .tag_name' | sort -V | tail -1)
 assets[0]="https://github.com/pi-hole/pi-hole/archive/$version.tar.gz"
-assets[1]="https://github.com/pi-hole/AdminLTE/archive/$version_adminlte.tar.gz"
+assets[1]="https://github.com/pi-hole/web/archive/$version_adminlte.tar.gz"
 assets[2]="https://github.com/pi-hole/FTL/archive/$version_ftl.tar.gz"
 
 # Later down the script, we assume the version has only digits and dots
