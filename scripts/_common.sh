decb7db592b2d4c29ba23190c2a99ceebeede616
Félix Piédallu <felix.piedallu@non.se.com>
2024-03-20 13:59:22 +0100

    update manifestv2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,16 +4,10 @@
 # COMMON VARIABLES
 #=================================================
 
-YNH_PHP_VERSION="7.4"
-
-php_dependencies="php$YNH_PHP_VERSION-common php$YNH_PHP_VERSION-cgi php$YNH_PHP_VERSION-sqlite3 php$YNH_PHP_VERSION-xml php$YNH_PHP_VERSION-intl"
-
-# dependencies used by the app (must be on a single line)
-pkg_dependencies="cmake build-essential libgmp-dev libidn11-dev nettle-dev libreadline-dev sqlite3 cron curl iputils-ping psmisc unzip idn2 libcap2-bin dns-root-data libcap2 netcat-openbsd procps jq $php_dependencies"
-
 pihole_adminlte_version=5.18
 pihole_flt_version=5.20
 
+# This is hard-coded upstream...
 PI_HOLE_LOCAL_REPO="/etc/.pihole"
 PI_HOLE_INSTALL_DIR="/opt/pihole"
 PI_HOLE_CONFIG_DIR="/etc/pihole"
@@ -23,21 +17,55 @@ PI_HOLE_BIN_DIR="/usr/local/bin"
 # PERSONAL HELPERS
 #=================================================
 
+_configure_ports() {
+    if [ "$port" -gt 4720 ]; then
+        ynh_die --message="The ports 4711 to 4720 are already in use. Pi-hole can't work on another port. Please try to free one of these ports."
+    fi
+
+    # Disable the port 53 for upnp
+    ynh_exec_fully_quiet yunohost firewall disallow Both 53 --no-reload
+    ynh_exec_fully_quiet yunohost firewall allow Both 53 --no-upnp
+
+    # Open the UDP port 67 for dhcp
+    ynh_exec_fully_quiet yunohost firewall allow UDP 67 --no-upnp
+}
+
+_add_cron_jobs() {
+    install -D -m 644 -T -o root -g root "$PI_HOLE_LOCAL_REPO/advanced/Templates/pihole.cron" /etc/cron.d/pihole
+
+    # Randomize gravity update time
+    ynh_replace_string --target_file="/etc/cron.d/pihole" \
+        --match_string="59 1 " \
+        --replace_string="$((1 + RANDOM % 58)) $((3 + RANDOM % 2)) "
+
+    # Randomize update checker time
+    ynh_replace_string --target_file="/etc/cron.d/pihole" \
+        --match_string="59 17" \
+        --replace_string="$((1 + RANDOM % 58)) $((12 + RANDOM % 8))"
+
+    # Remove git usage for version. Which fails because we use here a release instead of master.
+    ynh_replace_string --target_file="/etc/cron.d/pihole" \
+        --match_string=".*updatechecker.*" \
+        --replace_string="#&"
+}
+
+_add_sudoers_config() {
+    install -m 0640 "$PI_HOLE_LOCAL_REPO/advanced/Templates/pihole.sudo" /etc/sudoers.d/pihole
+    echo "$app ALL=NOPASSWD: ${PI_HOLE_BIN_DIR}/pihole" >> /etc/sudoers.d/pihole
+}
+
+_add_logrotate_config() {
+    install -D -m 644 -T "${PI_HOLE_LOCAL_REPO}"/advanced/Templates/logrotate "$PI_HOLE_CONFIG_DIR/logrotate"
+    sed -i "/# su #/d;" "$PI_HOLE_CONFIG_DIR/logrotate"
+}
+
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
 
 ynh_maintenance_mode_ON () {
-	# Load value of $path_url and $domain from the config if their not set
-	if [ -z $path_url ]; then
-		path_url=$(ynh_app_setting_get $app path)
-	fi
-	if [ -z $domain ]; then
-		domain=$(ynh_app_setting_get $app domain)
-	fi
-
 	mkdir -p /var/www/html/
-	
+
 	# Create an html to serve as maintenance notice
 	echo "<!DOCTYPE html>
 <html>
@@ -60,10 +88,10 @@ ynh_maintenance_mode_ON () {
 </html>" > "/var/www/html/maintenance.$app.html"
 
 	# Create a new nginx config file to redirect all access to the app to the maintenance notice instead.
-	echo "# All request to the app will be redirected to ${path_url}_maintenance and fall on the maintenance notice
-rewrite ^${path_url}/(.*)$ ${path_url}_maintenance/? redirect;
+	echo "# All request to the app will be redirected to ${path}_maintenance and fall on the maintenance notice
+rewrite ^${path}/(.*)$ ${path}_maintenance/? redirect;
 # Use another location, to not be in conflict with the original config file
-location ${path_url}_maintenance/ {
+location ${path}_maintenance/ {
 alias /var/www/html/ ;
 
 try_files maintenance.$app.html =503;
@@ -74,7 +102,7 @@ include conf.d/yunohost_panel.conf.inc;
 
 	# The current config file will redirect all requests to the root of the app.
 	# To keep the full path, we can use the following rewrite rule:
-	# 	rewrite ^${path_url}/(.*)$ ${path_url}_maintenance/\$1? redirect;
+	# 	rewrite ^${path}/(.*)$ ${path}_maintenance/\$1? redirect;
 	# The difference will be in the $1 at the end, which keep the following queries.
 	# But, if it works perfectly for a html request, there's an issue with any php files.
 	# This files are treated as simple files, and will be downloaded by the browser.
@@ -84,16 +112,8 @@ include conf.d/yunohost_panel.conf.inc;
 }
 
 ynh_maintenance_mode_OFF () {
-	# Load value of $path_url and $domain from the config if their not set
-	if [ -z $path_url ]; then
-		path_url=$(ynh_app_setting_get $app path)
-	fi
-	if [ -z $domain ]; then
-		domain=$(ynh_app_setting_get $app domain)
-	fi
-
-	# Rewrite the nginx config file to redirect from ${path_url}_maintenance to the real url of the app.
-	echo "rewrite ^${path_url}_maintenance/(.*)$ ${path_url}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
+	# Rewrite the nginx config file to redirect from ${path}_maintenance to the real url of the app.
+	echo "rewrite ^${path}_maintenance/(.*)$ ${path}/\$1 redirect;" > "/etc/nginx/conf.d/$domain.d/maintenance.$app.conf"
 	systemctl reload nginx
 
 	# Sleep 4 seconds to let the browser reload the pages and redirect the user to the app.
